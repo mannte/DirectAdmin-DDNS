6c152c5bfd37cb2f5e9716c31d1e40eb56e9a4c2
Brecht Wyns <brechtwyns@gmail.com>
2024-04-18 03:23:49 +0200

    Prioritize IPv4 over IPv6
    ipecho.net uses IPv6 as default. However, if you want to use this code to host your own DDNS solution, you probably want to use IPv4.

--- dyndns.php
+++ dyndns.php
@@ -24,6 +24,7 @@ $timeout = 5;
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
 
 $publicip = curl_exec($ch);
 
